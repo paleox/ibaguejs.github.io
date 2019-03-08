4ec5220e4cfa31a78f140bd5b144307c8db32627
Diego Avila <Diegoavilap@users.noreply.github.com>
2019-03-08 01:35:17 -0500

    Version 2019 1.0
    * Vue added for show events dynamically
    
    * Refactorization of appearance and added logos

--- main.js
+++ main.js
@@ -1,33 +1,67 @@
-var getMeetupData = function(url, data) {
-  return new Promise(function(resolve, reject) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('get', url, true);
-    xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
-    xhr.responseType = 'jsonp';
-    xhr.onload = function() {
-      var status = xhr.status;
-      if (status == 200) {
-        resolve(xhr.response);
-      } else {
-        reject(status);
-      }
-    };
-    xhr.send();
-  });
-};
-
 document.addEventListener('DOMContentLoaded', function() {
   // Get upcomming events
-  getMeetupData('https://api.meetup.com/IbagueJS/events?callback=data&sign=true&photo-host=public&page=5&desc=true&status=upcoming').then(function (data) {
-    console.log(data)
-  }).catch(function (err) {
-    console.error('Imposible to retrieve data from Meetup', err.statusText)
-  })
-
-  // Get past events
-  getMeetupData('https://api.meetup.com/IbagueJS/events?desc=true&photo-host=public&page=5&sig_id=58958882&status=past&sig=ed0ea86cdefed016050db8ddc35b7f94fc0490c6').then(function (data) {
-    console.log(data)
-  }).catch(function (err) {
-    console.error('Imposible to retrieve data from Meetup', err.statusText)
-  })
+  $.ajax({
+    url: "https://api.meetup.com/IbagueJS/events?desc=true&photo-host=public&page=5&sig_id=223811643&status=upcoming&sig=88be441521b1fab5ab1330461aa7a9a0101f86b5",
+    jsonp: "callback",
+    dataType: "jsonp",
+    data: {
+      format: "json"
+    },
+    success: function (result) {
+      console.log(result.data)
+      var events_upcoming = new Vue({
+        el: '#events_upcoming',
+        data: {
+          events_upcoming: result.data
+        }
+      })
+    },
+    fail: function (err) {
+      console.error('Imposible to retrieve data from Meetup', err.status)
+    }
+  });
+  //Get past events
+  $.ajax({
+    url: "https://api.meetup.com/IbagueJS/events?desc=true&photo-host=public&page=5&sig_id=223811643&status=past&sig=92a8e04546d333cb9eef865344831eb1a284ee47",
+    jsonp: "callback",
+    dataType: "jsonp",
+    data: {
+      format: "json"
+    },
+    success: function (result) {
+      console.log(result.data)
+      var events_past = new Vue({
+        el: '#events_past',
+        data: { 
+          events_past: result.data
+        }
+      })
+    },
+    fail: function (err) {
+      console.error('Imposible to retrieve data from Meetup', err.status)
+    }
+  });
+  //Get Organizers
+  $.ajax({
+    url: "https://api.meetup.com/IbagueJS/members?photo-host=public&role=leads&page=20&sig_id=223811643&sig=292d4f45b3d63e0d3f2bf0718f6f50c89a1e7f26",
+    jsonp: "callback",
+    dataType: "jsonp",
+    data: {
+      format: "json"
+    },
+    success: function (result) {
+      console.log(result.data)
+      var events_past = new Vue({
+        el: '#organizers',
+        data: {
+          organizers: result.data
+        }
+      })
+    },
+    fail: function (err) {
+      console.error('Imposible to retrieve data from Meetup', err.status)
+    }
+  });
 }, false)
+
+
