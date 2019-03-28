5063a58810d702dc002f70d8520c8556d34cf106
Andrés Felipe Santos Lozano <andresfesantos@gmail.com>
2019-03-28 17:44:13 -0500

    Refactoring main component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,56 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+<template lang="pug">
+  #app(:class="{ loaded: loader }")
+    app-loader
+    app-header
+    router-view
+    app-footer
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import AppLoader from '@/components/shared/AppLoader.vue'
+import AppHeader from '@/components/layout/AppHeader.vue'
+import AppFooter from '@/components/layout/AppFooter.vue'
+
+export default {
+  components: {
+    AppLoader,
+    AppHeader,
+    AppFooter
+  },
+  data() {
+    return {
+      loader: false
     }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.loader = true
+    }, 2000)
   }
+}
+</script>
+
+<style lang="scss">
+.is-js,
+.has-background-js {
+  background: #ffe400 !important;
+  border-color: #ffe400 !important;
+}
+
+.has-background-js-primary {
+  background: #375a7f !important;
+  border-color: #375a7f !important;
+}
+
+.has-padding {
+  padding-left: 5%;
+  padding-right: 7%;
+}
+
+.image-128x128 {
+  width: 128px !important;
+  height: 128px !important;
+  margin-left: 5px;
 }
 @import '@/scss/main.scss';
 </style>
