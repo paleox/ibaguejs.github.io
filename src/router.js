be0de4664173a26975226fa88462ee88d9623770
Andrés Felipe Santos Lozano <andresfesantos@gmail.com>
2019-03-28 17:48:43 -0500

    Adding route for landing view component

--- src/router.js
+++ src/router.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Landing from '@/views/Landing.vue'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: []
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Landing
+    }
+  ]
 })
