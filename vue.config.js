f15eca11d21f75e415aa33ec9e4fc41ee2b812b5
diegoavilap <diegoavilapava@hotmail.com>
2019-03-29 14:49:09 -0500

    Fixing publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 process.env.VUE_APP_MEETUP_URL = 'https://api.meetup.com/IbagueJS'
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/ibaguejs.com/' : '/'
+  publicPath: '/'
 }
