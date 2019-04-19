817bbce97405a0cfca9fe8113ffcb0236419b1c5
Andrés Felipe Santos Lozano <andresfesantos@gmail.com>
2019-04-18 22:17:39 -0500

    Remove Bulma. Adding and config Vuetify

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
+import '@/plugins/vuetify'
 import App from '@/App.vue'
 import router from '@/router'
+
+import '@fortawesome/fontawesome-free/css/all.css'
 
 Vue.config.productionTip = false
 
